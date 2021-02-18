e0433c2421ad52e9352294a9762d06206fb3be7a
우영창 <dudckd@uyeongchang-ui-MacBookPro.local>
2021-02-18 21:38:43 +0900

    message

--- src/components/ChatPage/MainPanel/MainPanel.js
+++ src/components/ChatPage/MainPanel/MainPanel.js
@@ -2,9 +2,51 @@ import React, { Component } from 'react'
 import MessageHeader from './MessageHeader';
 import MessageForm from './MessageForm';
 import Message from './Message';
+import {connect} from "react-redux";
+import firebase from "../../../firebase";
 
 export class MainPanel extends Component {
+
+    state = {
+        messages: [],
+        messagesRef: firebase.database().ref("messages"),
+        messagesLoading: true,
+    }
+    
+
+    componentDidMount() {
+        const { chatRoom } = this.props;
+
+        if (chatRoom) {
+            this.addMessagesListeners(chatRoom.id)
+        }
+    }
+
+    addMessagesListeners = (chatRoomId) => {
+        let messagesArray = []; 
+        this.state.messagesRef.child(chatRoomId).on("child_added", DataSnapshot => {
+            messagesArray.push(DataSnapshot.val());
+            this.setState({
+                messages: messagesArray,
+                messagesLoading: false
+            })
+        })
+    }
+    
+    
+    renderMessages = (messages) =>
+        messages.length > 0 &&
+        messages.map(message => (
+            <Message
+                key={message.timestamp}
+                message={message}
+                user={this.props.user}
+            />
+        ))
+
     render() {
+
+        const { messages } = this.state;
         return (
             <div style={{ padding:'2rem 2rem 0 2rem'}}>
 
@@ -18,8 +60,11 @@ export class MainPanel extends Component {
                     padding:'1rem',
                     marginBottom:'1rem',
                     overflowY:'auto'
-                }}>
-
+                }}> 
+                    {
+                        this.renderMessages(messages)
+                    }                    
+                
                 </div>
                 
                 <MessageForm />
@@ -29,4 +74,11 @@ export class MainPanel extends Component {
     }
 }
 
-export default MainPanel
+const mapStateToProps = state => {
+    return {
+        user: state.user.currentUser,
+        chatRoom: state.chatRoom.currentChatRoom
+    }
+}
+
+export default connect(mapStateToProps)(MainPanel)
--- src/components/ChatPage/MainPanel/MessageForm.js
+++ src/components/ChatPage/MainPanel/MessageForm.js
@@ -1,24 +1,86 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Form from "react-bootstrap/Form"
 import ProgressBar from "react-bootstrap/ProgressBar"
 import Row from "react-bootstrap/Row"
 import Col from "react-bootstrap/Col"
+import firebase from "../../../firebase";
+import { useSelector } from 'react-redux'
 
 
 function MessageForm() {
+
+    const [content, setcontent] = useState("")
+    const [errors, seterrors] = useState([])
+    const [loading, setloading] = useState(false)
+    const messagesRef = firebase.database().ref("messages")
+    const chatRoom = useSelector(state => state.chatRoom.currentChatRoom)
+    const user = useSelector(state => state.user.currentUser)
+
+    const handleChange =(e) => {
+        setcontent(e.target.value)
+    }
+
+    const createMessage =(fileUrl = null)=>{
+        const message = {
+            timestamp :firebase.database.ServerValue.TIMESTAMP,
+            user: {
+                id:user.uid,
+                name: user.displayName,
+                image:user.photoURL
+            }
+        }
+
+        if(fileUrl !== null) {
+            message["image"] = fileUrl;
+        }else{
+            message["content"] = content;
+        }
+        return message;
+
+    }
+
+    const handleSubmit = async ()=>{
+        if(!content){
+            seterrors(prev=> prev.concat("Type contents first"))
+            return;
+        }
+        setloading(true);
+        //firebase 에 저장
+        try { 
+            await messagesRef.child(chatRoom.id).push().set(createMessage())
+            setloading(false)
+            setcontent("")
+            seterrors([])
+        } catch (error) {
+            seterrors(pre => pre.concat(error.message))
+            setloading(false)
+            setTimeout(() => {
+                seterrors([])
+            }, 5000);
+        }
+    }
+
     return (
         <div>
-            <Form>
+            <Form onSubmit={handleSubmit}>
                 <Form.Group controlId="exampleForm.ControlTextarea1">
-                    <Form.Control as="textarea" rows={3} />
+                    <Form.Control
+                    value={content}
+                    onChange={handleChange}
+                    as="textarea" rows={3} />
                 </Form.Group>
             </Form>
 
             <ProgressBar variant="warning" label="60%" now={60} />
 
+            <div>
+                {errors.map(errorMsg => <p style={{color:"red"}} key={errorMsg}>{errorMsg}</p>)}
+            </div>
+
             <Row>
                 <Col>
                     <button 
+                    onClick={handleSubmit}
                     className="message-form-button"
                     style={{width:'100%'}}>
                         SEND
